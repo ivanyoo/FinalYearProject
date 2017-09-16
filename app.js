eb32df38b328faf16842f7ebf1276f0f7394a4d6
ivanyoo <114344211@umail.ucc.ie>
2017-09-16 19:05:26 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(8081);
+server.listen(80);
 let userid = 1;
 
 app.get('/', (req, res) => {
@@ -11,12 +11,13 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-   socket.emit('news', {hello: 'world', id: userid});
-   socket.on('my other event', (data) => {
-      console.log(data);
-   });
-
    socket.on('answerEvent', (data) => {
       console.log(`Player ${data.id} answered ${data.answer}`);
+      io.to('testroom').emit('roomevent');
+   });
+
+   socket.on('joinRoomEvent', () => {
+      console.log('joined');
+      socket.join('testroom');
    });
 });
