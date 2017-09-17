06b2bc398c9247315ae016e07724545820e30695
ivanyoo <114344211@umail.ucc.ie>
2017-09-17 15:16:53 +0100

    test

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const Queue = require('./Queue.src');
 
 server.listen(80);
 let userid = 1;
+let roomNumber = 1;
+const playerQueue = new Queue();
 
 app.get('/', (req, res) => {
-   userid++;
    res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', (socket) => {
-   socket.on('answerEvent', (data) => {
-      console.log(`Player ${data.id} answered ${data.answer}`);
-      io.to('testroom').emit('roomevent');
+   let sessionRoomNumber;
+   let answers = [];
+
+   socket.on(`answerEvent`, (data) => {
+      answers.push(data.answer);
+      io.to(`testroom-${sessionRoomNumber}`).emit('opponentAnswerEvent', {answer: data.answer});
    });
 
-   socket.on('joinRoomEvent', () => {
-      console.log('joined');
-      socket.join('testroom');
+   socket.on('opponentAnswerEvent', (data) => {
+      if (answers.includes(data.answer)) {
+         io.to(`testroom-${sessionRoomNumber}`).emit('answerMatchEvent');
+      }
+   });
+
+   socket.on('joinRoomEvent', (data) => {
+      socket.join(`testroom-${data.roomNumber}`);
+      socket.emit('roomJoinedEvent');
+});
+
+   socket.on('findRoomEvent', (data) => {
+      userid++;
+      if (playerQueue.getLength() > 0) {
+         sessionRoomNumber = roomNumber;
+         roomNumber++;
+         playerQueue.dequeue().emit('roomFoundEvent', {sessionRoomNumber: sessionRoomNumber});
+         socket.emit('roomFoundEvent', {sessionRoomNumber: sessionRoomNumber});
+      } else {
+         playerQueue.enqueue(socket);
+      }
    });
 });
